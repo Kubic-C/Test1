6da8cf356b0fe0d3430c78f5c5749ff3ece503df
PythonCoderforlife <sawyerp06@gmail.com>
2020-01-04 23:34:13 -0600

    f

--- Test1/Test1.py
+++ Test1/Test1.py
@@ -2,11 +2,19 @@
 Num1 = float(input("Please put in a number :"))
 Num2 = float(input("Please put in a second number :"))
 Operater = input("Put in your operator(Ex. - , x , + , or /):")
-
+#YO BUDDY
 def Caculator1(Num1, Num2, Operator):
     if Operater == "-":
         print(Num1 - Num2)
-    elif Operator == "x"
+    elif Operator == "x":
+        print(Num1 * Num2)
+    elif Operater == "+":
+        print(Num1 + Num2)
+    elif Operater == "/":
+        prinnt(Num1 / Num2)
+    else:
+        print("Inv")
+
 
 
 
